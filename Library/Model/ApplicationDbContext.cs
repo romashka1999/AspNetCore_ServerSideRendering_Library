1cb75f8039917aec208b436be91fe54749be7bb3
Your Namect git config --global user.email <roma.chikhladze777@gmail.com>
2020-04-30 15:56:18 +0400

    some functionality added on book

---
+++ Library/Model/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Book> Book { get; set; }
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
