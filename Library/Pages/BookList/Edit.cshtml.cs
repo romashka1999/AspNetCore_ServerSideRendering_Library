b8a913b6a027e4d443ad0756a562f540699393f0
Your Namect git config --global user.email <roma.chikhladze777@gmail.com>
2020-05-01 13:00:35 +0400

    booklist CRUD is completed

---
+++ Library/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Library
+{
+    public class EditModel : PageModel
+    {
+
+        private ApplicationDbContext _db;
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var bookFromDb = await _db.Book.FindAsync(Book.Id);
+                bookFromDb.Name = Book.Name;
+                bookFromDb.Author = Book.Author;
+                bookFromDb.ISBN = Book.ISBN;
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
